805682cf19029b40d7ec5ab3db311aaa252cb57a
Kyle Geske <stungeye@gmail.com>
2018-10-23 13:52:10 -0500

    Changing Wally to Kyle.

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
 <head>
   <meta charset="utf-8" />
-  <title>Wally's Favourite Website</title>
+  <title>Kyle's New Site</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
   <script src="main.js"></script>
 </head>
 <body>
-  <h1>Wally's Website</h1>
+  <h1>Kyle's Website</h1>
   <div class="gallery">
     <img src="" alt="">
     <img src="" alt="">
